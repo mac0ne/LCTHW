4c9354aa4ae7b37832dcb57f963464432174645f
mac0ne <wsq302@gmail.com>
2016-01-14 16:16:11 +0000

    test

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int n = 1000000;
+    char c = '@';
+    float money=93.96;
+    printf("n=%o,c=%c,money=%2.3f\n",n,c,money);
+    puts("\61\62\63\141\142\143");
+    puts("C\tC++\tJava\nC first appeared!\a");
+    return 0;
+}
